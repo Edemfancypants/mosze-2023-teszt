e4098191c727e6dc048394bdc5a42dec6e14d8db
Edemfancypants <adampoczok@gmail.com>
2023-09-24 18:30:35 +0200

    Fixed mistakes, tidied up commenting

--- main.cpp
+++ main.cpp
@@ -5,34 +5,34 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     //typo NELEMENTS =/= N_ELEMENTS
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
 
-    // missing ;
-    std::cout << '1-100 ertekek duplazasa'
+    // missing ;, sor nincs lezárva, '' =/= ""
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
 
     //for loop syntax errors
-    for (int i = 0;) 
+    for (int i = 0; i < N_ELEMENTS; I++) 
     {
-        //1-100 * 2
-        b[i] = i * 2; 
+        //0-99-ig fut a loop. 1-100-ig kell
+        b[i] = (i+1) * 2; 
     }
     //hibás ciklus vezérlő
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        //az értékváltozó nem kerül kiirásra, hiányzik a ;
-        std::cout << "Ertek:"
+        //az értékváltozó nem kerül kiirásra, hiányzik a ;, a sor nem kerül lezárásra
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
 
     std::cout << "Atlag szamitasa: " << std::endl;
 
     //nincs kezdőértéke a változónak
-    int atlag;
+    int atlag = 0;
 
     //missing ;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //missing ;
-        atlag += b[i]
+        atlag += b[i];
     }
 
     atlag /= N_ELEMENTS;
